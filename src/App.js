f6830a474e5612a3203d2eb09b317f327f7f1b73
Chad Smith <chad@codingbychad.com>
2020-12-28 16:33:53 -0600

    formatted layout for display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import './App.css';
 import { P } from './components/paragraph/Paragraph';
 import { H1, H2, H3, H4, H5, H6 } from './components/header/Header';
+import { UL, OL } from './components/lists/lists';
 
 function App() {
   return (
     <div>
-      <h1>As You Wish Ipsum Examples</h1>
+      <header>As You Wish Ipsum Examples</header>
       <section>
         <article>
-          <h1>Headers</h1>
+          <h2>Headers</h2>
+          <span>H1</span>
           <H1/>
+          <span>H2</span>
           <H2 />
+          <span>H3</span>
           <H3 />
+          <span>H4</span>
           <H4 />
+          <span>H5</span>
           <H5 />
+          <span>H6</span>
           <H6 />
         </article>
-      </section>
-      <section>
         <article>
-          <h1>Paragraphs</h1>
+          <h2>Paragraphs</h2>
           <P />
+        </article>
+        <article>
+          <h2>Lists</h2>
+          <h4>Unordered List:</h4>
+          <UL />
+          <h4>Ordered List:</h4>
+          <OL />
         </article>
       </section>
     </div>
